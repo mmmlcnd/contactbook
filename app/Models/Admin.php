db434037df29bcfb80b8b6987e76a4525b235525
mmmlcnd <m.murai9180@gmail.com>
2025-10-17 07:33:27 +0900

    Admin.phpからattemptLogin()をコメントアウト

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -14,29 +14,29 @@ class Admin
     }
 
     // 管理者をIDで取得
-    public function find($id)
+    public function find($id) // 指定されたIDに対応するレコードを取得
     {
         $stmt = $this->pdo->prepare("SELECT * FROM admins WHERE id = ?");
         $stmt->execute([$id]);
         return $stmt->fetch(PDO::FETCH_OBJ);
     }
 
-    // メールアドレスで認証
-    public function attemptLogin($email, $password)
-    {
-        $stmt = $this->pdo->prepare("SELECT * FROM admins WHERE email = ?");
-        $stmt->execute([$email]);
-        $admin = $stmt->fetch(PDO::FETCH_OBJ);
+    // メールアドレスで認証※Auth.phpに記述
+    // public function attemptLogin($email, $password)
+    // {
+    //     $stmt = $this->pdo->prepare("SELECT * FROM admins WHERE email = ?");
+    //     $stmt->execute([$email]);
+    //     $admin = $stmt->fetch(PDO::FETCH_OBJ);
 
-        if ($admin && password_verify($password, $admin->password)) {
-            return $admin;
-        }
+    //     if ($admin && password_verify($password, $admin->password)) {
+    //         return $admin;
+    //     }
 
-        return null;
-    }
+    //     return null;
+    // }
 
     // 新規作成
-    public function create($data)
+    public function create($data) // 新しいレコードをデータベースに作成
     {
         $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
         $stmt = $this->pdo->prepare("INSERT INTO admins (name, kana, email, password, created_at, updated_at)
